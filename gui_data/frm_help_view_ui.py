3ee0a908c6a551a0a9e64677082186adb5f910cf
SuccuS-VMB <oe4vmb@gmail.com>
2025-11-22 20:07:49 +0100

    manual written

--- gui_data/frm_help_view_ui.py
+++ gui_data/frm_help_view_ui.py
@@ -14,7 +14,8 @@ from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
+    QPalette, QPixmap, QRadialGradient, QTransform,
+    QTextCharFormat, QTextFormat) 
 from PySide6.QtWidgets import (QApplication, QDialog, QHBoxLayout, QSizePolicy,
     QTextBrowser, QWidget)
 
@@ -28,6 +29,31 @@ class Ui_frm_help_view(object):
         self.textBrowser = QTextBrowser(frm_help_view)
         self.textBrowser.setObjectName(u"textBrowser")
 
+        # NEUE KORREKTUREN FÜR DEN TEXTBROWSER START
+        
+        # Cursor des QTextBrowser-Dokuments holen
+        cursor = self.textBrowser.textCursor()
+        
+        # 1. Zeichen-Formatierung anpassen (Setzt die Standard-Schrift)
+        char_format = QTextCharFormat()
+        char_format.setFontFamily("Arial")
+        cursor.setCharFormat(char_format)
+        
+        # 2. Block-Formatierung anpassen, um die Zeilenhöhe zu erzwingen
+        block_format = cursor.blockFormat()
+        
+        # KORREKTUR: Ersetzt QTextFormat.ProportionalHeight (welches einen Fehler warf)
+        # durch seinen Integer-Wert 3. Dies erzwingt die proportionale Zeilenhöhe von 160%.
+        block_format.setLineHeight(160, 3) 
+        
+        cursor.setBlockFormat(block_format)
+
+        # 3. Das gesamte Dokument mit den neuen Formaten aktualisieren
+        self.textBrowser.setCurrentCharFormat(char_format)
+        self.textBrowser.setTextCursor(cursor)
+        
+        # NEUE KORREKTUREN FÜR DEN TEXTBROWSER ENDE
+
         self.horizontalLayout.addWidget(self.textBrowser)
 
 
@@ -39,4 +65,3 @@ class Ui_frm_help_view(object):
     def retranslateUi(self, frm_help_view):
         frm_help_view.setWindowTitle(QCoreApplication.translate("frm_help_view", u"Dialog", None))
     # retranslateUi
-
